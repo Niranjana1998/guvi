d4a1a56e9adcb3274e97ac3274158e6274a200db
Niranjana1998 <52449393+Niranjana1998@users.noreply.github.com>
2019-07-04 10:03:00 +0530

    Update prg3.py

--- prg3.py
+++ prg3.py
@@ -1,7 +1,7 @@
 alpha=str(input())
 if(alpha == 'a' or alpha == 'e' or alpha == 'i' or alpha == 'o' or alpha == 'u' or alpha == 'A'
        or alpha == 'E' or alpha == 'I' or alpha == 'O' or alpha == 'U'):
-    print("vowel")
+    print("Vowel")
 elif(alpha>='a' and alpha<='z')or(alpha>='A' and alpha<='Z'):
     print("Consonant")
 else:
