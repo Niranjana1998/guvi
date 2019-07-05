1c93354024c433df5ef4f5569070bd72fb93d4f0
Niranjana1998 <52449393+Niranjana1998@users.noreply.github.com>
2019-07-05 12:21:01 +0530

    Update and rename prg14.py to prg14a.py

--- prg14.py
+++
@@ -1,9 +0,0 @@
-start=int(input())
-end = int(input())
-  
-# iterstartting estartch xender in list 
-for x in range(start, end + 1): 
-      
-    # checking coendition 
-    if x % 2 != 0: 
-        print(x, end = " ")
